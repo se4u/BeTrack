faaa47e56869519dbbe78af4bf748939647d4423
cedric <cedoctet@gmail.com>
2016-09-20 15:36:50 +0200

    dev ongoign

--- web_sql_hagen/BeTrackPostDailyStatus.php
+++ web_sql_hagen/BeTrackPostDailyStatus.php
@@ -7,10 +7,13 @@ if (mysqli_connect_errno($con))
 
 $userid = $_POST['ParticipantID'];
 $periodstatus = $_POST['Period'];
+$sociallife1 = $_POST['SocialLife1'];
+$sociallife2 = $_POST['SocialLife2'];
+$mood = $_POST['Mood'];
 $date = $_POST['Date'];
 
-$result = mysqli_query($con,"INSERT INTO BetrackDailyStatus (UserId, PeriodStatus, Date) 
-          VALUES ('$userid ', '$periodstatus', '$date')");
+$result = mysqli_query($con,"INSERT INTO BetrackDailyStatus (UserId, PeriodStatus, SocialLife1, SocialLife2, Mood, Date) 
+          VALUES ('$userid ', '$periodstatus', '$sociallife1', '$sociallife2', '$mood', '$date')");
  
 if($result == true) {
     echo '{"query_result":"SUCCESS"} ';
